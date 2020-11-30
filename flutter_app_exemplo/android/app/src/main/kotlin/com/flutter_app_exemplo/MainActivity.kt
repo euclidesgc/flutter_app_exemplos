863c9b6d56e21be0dda17af6112c3be242e08002
rodrigo.silveira <rodrigo.silveira@b2wdigital.com>
2020-11-30 16:57:20 -0300

    first commit

---
+++ flutter_app_exemplo/android/app/src/main/kotlin/com/flutter_app_exemplo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_app_exemplo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app_exemplo/lib/main.dart
@@ -0,0 +1,43 @@
+
+import 'package:components/util/theme_util.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:models/login/UserModel.dart';
+import 'package:onbording/login/ui/loginPage.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeUtil.getThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),
+    body: _body(),);
+  }
+
+  _body() {
+    return Stack(children: [
+      LoginPage(),
+      RaisedButton(onPressed: (){
+       UserModel userModel = Get.find();
+       debugPrint(userModel.nome);
+      })
+    ],);
+
+  }
+}
+
